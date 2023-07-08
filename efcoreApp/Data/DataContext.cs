0621251509492e02e2a46ac4b68d5bc6d6f0361e
“sadikturan” <sadikturan@gmail.com>
2023-07-08 17:35:39 +0300

    migrations

--- efcoreApp/Data/DataContext.cs
+++ efcoreApp/Data/DataContext.cs
@@ -4,6 +4,10 @@ namespace efcoreApp.Data
 {
     public class DataContext: DbContext 
     {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
         public DbSet<Kurs> Kurslar => Set<Kurs>();
         public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
         public DbSet<KursKayit> KursKayitlari => Set<KursKayit>();
---
+++ efcoreApp/Migrations/20230708134420_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace efcoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KursKayitlari",
+                columns: table => new
+                {
+                    KayitId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OgrenciId = table.Column<int>(type: "INTEGER", nullable: false),
+                    KursId = table.Column<int>(type: "INTEGER", nullable: false),
+                    KayitTarihi = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KursKayitlari", x => x.KayitId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kurslar",
+                columns: table => new
+                {
+                    KursId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Baslik = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kurslar", x => x.KursId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ogrenciler",
+                columns: table => new
+                {
+                    OgrenciId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OgrenciAd = table.Column<string>(type: "TEXT", nullable: true),
+                    OgrenciSoyad = table.Column<string>(type: "TEXT", nullable: true),
+                    Eposta = table.Column<string>(type: "TEXT", nullable: true),
+                    Telefon = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogrenciler", x => x.OgrenciId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KursKayitlari");
+
+            migrationBuilder.DropTable(
+                name: "Kurslar");
+
+            migrationBuilder.DropTable(
+                name: "Ogrenciler");
+        }
+    }
+}
