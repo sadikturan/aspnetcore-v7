66463aa65e23a89d1cd4265178c99e3e7c5d6ba2
“sadikturan” <sadikturan@gmail.com>
2023-07-08 13:05:11 +0300

    dbcontext

---
+++ efcoreApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Data
+{
+    public class DataContext: DbContext 
+    {
+        public DbSet<Kurs> Kurslar => Set<Kurs>();
+        public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
+        public DbSet<KursKayit> KursKayitlari => Set<KursKayit>();
+    }
+
+    // code-first => entity, dbcontext => database (sqlite)
+    // database-first => sql server
+}
