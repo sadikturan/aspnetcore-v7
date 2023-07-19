f909582712a0f09a5cc3735015962dd22619c544
“sadikturan” <sadikturan@gmail.com>
2023-07-19 12:36:37 +0300

    kurs silme

--- efcoreApp/Controllers/KursController.cs
+++ efcoreApp/Controllers/KursController.cs
@@ -82,5 +82,36 @@ namespace efcoreApp.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id);
+
+            if(kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm]int id)
+        {
+            var kurs = await _context.Kurslar.FindAsync(id);
+            if(kurs == null)
+            {
+                return NotFound();
+            }
+            _context.Kurslar.Remove(kurs);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
