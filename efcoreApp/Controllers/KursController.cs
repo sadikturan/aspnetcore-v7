4cceae4b3f2dfc8abb0bf3ea871b0ca4b68f5d99
“sadikturan” <sadikturan@gmail.com>
2023-07-24 07:36:35 +0300

    kurs öğrencilerinin listelenmesi

--- efcoreApp/Controllers/KursController.cs
+++ efcoreApp/Controllers/KursController.cs
@@ -40,7 +40,11 @@ namespace efcoreApp.Controllers
                 return NotFound();
             }
 
-            var kurs = await _context.Kurslar.FindAsync(id);
+            var kurs = await _context
+                        .Kurslar
+                        .Include(k => k.KursKayitlari)
+                        .ThenInclude(k=> k.Ogrenci)
+                        .FirstOrDefaultAsync(k => k.KursId == id);
 
             if(kurs == null) 
             {
--- efcoreApp/Data/Kurs.cs
+++ efcoreApp/Data/Kurs.cs
@@ -4,5 +4,7 @@ namespace efcoreApp.Data
     {
         public int KursId { get; set; }
         public string? Baslik { get; set; }
+
+        public ICollection<KursKayit> KursKayitlari { get; set; } = new List<KursKayit>();
     }
 }
