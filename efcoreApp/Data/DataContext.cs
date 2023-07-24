f88f922f4043fcd7553179e32546300cddd6ba6a
“sadikturan” <sadikturan@gmail.com>
2023-07-24 22:03:38 +0300

    veritabani semasinin guncellenmesi

--- efcoreApp/Data/DataContext.cs
+++ efcoreApp/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace efcoreApp.Data
         public DbSet<Kurs> Kurslar => Set<Kurs>();
         public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
         public DbSet<KursKayit> KursKayitlari => Set<KursKayit>();
+        public DbSet<Ogretmen> Ogretmenler => Set<Ogretmen>();
     }
 }
---
+++ efcoreApp/Data/Ogretmen.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace efcoreApp.Data
+{
+    public class Ogretmen
+    {
+        [Key]
+        public int OgretmenId { get; set; }
+        public string? Ad { get; set; }
+        public string? Soyad { get; set; }
+        public string? Eposta { get; set; }
+        public string? Telefon { get; set; }
+        public DateTime BaslamaTarihi { get; set; }
+        public ICollection<Kurs> Kurslar { get; set; } = new List<Kurs>();
+    }
+}
