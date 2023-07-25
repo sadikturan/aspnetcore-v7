57894af675c6f96e5a0ce9c30d8f8d9560589f3d
“sadikturan” <sadikturan@gmail.com>
2023-07-25 08:37:25 +0300

    öğretmen yönetim sayfaları

--- efcoreApp/Data/Ogretmen.cs
+++ efcoreApp/Data/Ogretmen.cs
@@ -10,6 +10,9 @@ namespace efcoreApp.Data
         public string? Soyad { get; set; }
         public string? Eposta { get; set; }
         public string? Telefon { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = false)]
         public DateTime BaslamaTarihi { get; set; }
         public ICollection<Kurs> Kurslar { get; set; } = new List<Kurs>();
     }
