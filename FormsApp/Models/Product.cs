c8759aba5381abe0a3af6bead870c84aabb8c685
“sadikturan” <sadikturan@gmail.com>
2023-07-30 12:41:46 +0300

    product list

--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FormsApp.Models
 {
     public class Product
     {
+        [Display(Name="Urun Id")]
         public int ProductId { get; set; }
+
+        [Display(Name="Urun Adı")]
         public string Name { get; set; } = string.Empty;
+
+        [Display(Name="Fiyat")]
         public decimal Price { get; set; }
+
+        [Display(Name="Resim")]
         public string Image { get; set; } = string.Empty;
         public bool IsActive { get; set; }
         public int CategoryId { get; set; }
--- FormsApp/Models/Repository.cs
+++ FormsApp/Models/Repository.cs
@@ -6,7 +6,16 @@ namespace FormsApp.Models
         private static readonly List<Category> _categories = new();
         static Repository()
         {
+            _categories.Add(new Category { CategoryId = 1, Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Bilgisayar" });
 
+            _products.Add(new Product {ProductId = 1, Name = "IPhone 14", Price = 40000, IsActive = true,Image = "1.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 2, Name = "IPhone 15", Price = 50000, IsActive = true,Image = "2.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 3, Name = "IPhone 16", Price = 60000, IsActive = true,Image = "3.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 4, Name = "IPhone 17", Price = 70000, IsActive = true,Image = "4.jpg",CategoryId = 1 });
+            
+            _products.Add(new Product {ProductId = 5, Name = "Macbook Air", Price = 80000, IsActive = true,Image = "5.jpg",CategoryId = 2 });
+            _products.Add(new Product {ProductId = 6, Name = "Macbook Pro", Price = 90000, IsActive = true,Image = "6.jpg",CategoryId = 2 });
         }
 
         public static List<Product> Products
