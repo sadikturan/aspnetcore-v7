054431d80bd8c08d76f43096f3fdb3ded93dd15a
“sadikturan” <sadikturan@gmail.com>
2023-07-31 22:27:54 +0300

    categories select

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
@@ -10,7 +11,7 @@ public class HomeController : Controller
     {
     }
 
-    public IActionResult Index(string searchString)
+    public IActionResult Index(string searchString, string category)
     {
         var products = Repository.Products;
 
@@ -19,6 +20,13 @@ public class HomeController : Controller
             ViewBag.SearchString = searchString;
             products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
         }
+
+        if(!String.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+        }
+
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View(products);
     }
 
