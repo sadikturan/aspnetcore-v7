ee3f2304521ae184fea9f6934193cc87c7a3f15e
“sadikturan” <sadikturan@gmail.com>
2023-08-23 12:25:25 +0300

    post details

--- BlogApp/BlogApp/Controllers/PostsController.cs
+++ BlogApp/BlogApp/Controllers/PostsController.cs
@@ -2,6 +2,7 @@ using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers
 {
@@ -20,6 +21,11 @@ namespace BlogApp.Controllers
                     Posts = _postRepository.Posts.ToList()
                 }
             );
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.PostId == id));
         }
     }
 }
