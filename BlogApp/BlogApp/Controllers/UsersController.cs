6415f9afedd0a1022e0a7da1fb18085bed7f8b03
“sadikturan” <sadikturan@gmail.com>
2023-09-09 21:38:11 +0300

    kullanıcı hesabının oluşturulması

--- BlogApp/BlogApp/Controllers/UsersController.cs
+++ BlogApp/BlogApp/Controllers/UsersController.cs
@@ -33,11 +33,26 @@ namespace BlogApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if(ModelState.IsValid)
             {
-                return RedirectToAction("Login");
+                var user = await _userRepository.Users.FirstOrDefaultAsync(x => x.UserName == model.UserName || x.Email == model.Email);
+                if(user == null)
+                {
+                    _userRepository.CreateUser(new User {
+                        UserName  = model.UserName,
+                        Name = model.Name,
+                        Email = model.Email,
+                        Password = model.Password,
+                        Image = "avatar.jpg"
+                    });
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username ya da Email kullanımda.");
+                }
             }
             return View(model);
         }
