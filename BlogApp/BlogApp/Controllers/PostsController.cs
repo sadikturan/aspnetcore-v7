5fdd34ee11bcdbf5069d3e6925515b99fc5e264d
“sadikturan” <sadikturan@gmail.com>
2023-09-11 22:20:00 +0300

    post listesi

--- BlogApp/BlogApp/Controllers/PostsController.cs
+++ BlogApp/BlogApp/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using BlogApp.Entity;
 using BlogApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -63,12 +64,14 @@ namespace BlogApp.Controllers
 
         }
 
+        [Authorize]
         public IActionResult Create()
         {
             return View();
         }   
 
-         [HttpPost]
+        [HttpPost]
+        [Authorize]
         public IActionResult Create(PostCreateViewModel model)
         {
             if(ModelState.IsValid)
@@ -90,5 +93,21 @@ namespace BlogApp.Controllers
             }
             return View(model);
         }       
+
+        [Authorize]
+        public async Task<IActionResult> List()
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "");
+            var role = User.FindFirstValue(ClaimTypes.Role);
+
+            var posts = _postRepository.Posts;
+
+            if(string.IsNullOrEmpty(role))
+            {
+                posts = posts.Where(i => i.UserId == userId);
+            }
+
+            return View(await posts.ToListAsync());
+        }  
     }
 }
--- BlogApp/BlogApp/Program.cs
+++ BlogApp/BlogApp/Program.cs
@@ -14,7 +14,9 @@ builder.Services.AddScoped<ITagRepository, EfTagRepository>();
 builder.Services.AddScoped<ICommentRepository, EfCommentRepository>();
 builder.Services.AddScoped<IUserRepository, EfUserRepository>();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
+    options.LoginPath = "/Users/Login";
+});
 
 var app = builder.Build();
 
