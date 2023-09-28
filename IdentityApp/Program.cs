67ed4cd47278a520613e2257b366b906018a052c
“sadikturan” <sadikturan@gmail.com>
2023-09-28 09:34:20 +0300

    identity options

--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddDbContext<IdentityContext>(
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
 
+builder.Services.Configure<IdentityOptions>(options => {
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
