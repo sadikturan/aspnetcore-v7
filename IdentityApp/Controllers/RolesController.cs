6833f03f31287f3938f3688056152388dfda10fa
“sadikturan” <sadikturan@gmail.com>
2023-09-30 09:25:56 +0300

    role ekleme

---
+++ IdentityApp/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public  class RolesController:Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(model);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+
+            }
+            return View(model);
+        }
+    }
+}
