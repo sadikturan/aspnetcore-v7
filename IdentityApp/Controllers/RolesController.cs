4c91edb3e261e308dc917bb3a37461e4cdfc5435
“sadikturan” <sadikturan@gmail.com>
2023-10-03 12:57:29 +0300

    role detay sayfası

--- IdentityApp/Controllers/RolesController.cs
+++ IdentityApp/Controllers/RolesController.cs
@@ -7,9 +7,11 @@ namespace IdentityApp.Controllers
     public  class RolesController:Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
-        public RolesController(RoleManager<AppRole> roleManager)
+        private readonly UserManager<AppUser> _userManager;
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -42,5 +44,50 @@ namespace IdentityApp.Controllers
             }
             return View(model);
         }
+    
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if(role != null && role.Name != null)
+            {
+                ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                return View(role);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppRole model)
+        {
+            if(ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.Id);
+
+                if(role != null)
+                {
+                    role.Name = model.Name;
+
+                    var result = await _roleManager.UpdateAsync(role);
+
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+
+                    if(role.Name != null)
+                        ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                }
+            }
+
+            return View(model);
+        }
+    
     }
 }
