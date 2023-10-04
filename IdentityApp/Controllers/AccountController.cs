7a6c5aeccb5fb13a113ac2d8456a3c77dfea8819
“sadikturan” <sadikturan@gmail.com>
2023-10-04 14:56:10 +0300

    login sayfası

---
+++ IdentityApp/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+using IdentityApp.Models;
+using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class AccountController:Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
