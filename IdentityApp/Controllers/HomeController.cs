eb7a8ea107072bbf7dcef115ca03587155d1b25b
“sadikturan” <sadikturan@gmail.com>
2023-10-04 11:36:40 +0300

    autentication

--- IdentityApp/Controllers/HomeController.cs
+++ IdentityApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IdentityApp.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -21,6 +21,16 @@ builder.Services.Configure<IdentityOptions>(options => {
 
     options.User.RequireUniqueEmail = true;
     // options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+});
+
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);
 });
 
 var app = builder.Build();
@@ -37,7 +47,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
