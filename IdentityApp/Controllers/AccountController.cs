502c1429b3d54ae59bd6ea5fc518b811b8bef769
“sadikturan” <sadikturan@gmail.com>
2023-10-04 19:39:07 +0300

    cookie authentication

--- IdentityApp/Controllers/AccountController.cs
+++ IdentityApp/Controllers/AccountController.cs
@@ -7,10 +7,61 @@ namespace IdentityApp.Controllers
 {
     public class AccountController:Controller
     {
+        private UserManager<AppUser> _userManager;
+        private RoleManager<AppRole> _roleManager;
+        private SignInManager<AppUser> _signInManager;
+        public AccountController(
+            UserManager<AppUser> userManager, 
+            RoleManager<AppRole> roleManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager; 
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user  = await _userManager.FindByEmailAsync(model.Email);
+
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe,true);
+
+                    if(result.Succeeded)
+                    {
+                        await _userManager.ResetAccessFailedCountAsync(user);
+                        await _userManager.SetLockoutEndDateAsync(user, null);
+
+                        return RedirectToAction("Index","Home");
+                    }
+                    else if(result.IsLockedOut)
+                    {
+                        var lockoutDate = await _userManager.GetLockoutEndDateAsync(user);
+                        var timeLeft = lockoutDate.Value - DateTime.UtcNow;
+                        ModelState.AddModelError("", $"Hesabınız kitlendi, Lütfen {timeLeft.Minutes} dakika sonra deneyiniz");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "parolanız hatalı");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "bu email adresiyle bir hesap bulunamadı");
+                }
+            }
+            return View(model);
+        }
     }
 }
