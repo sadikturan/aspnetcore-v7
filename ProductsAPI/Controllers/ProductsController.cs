3c97de4fddca9966d1adb0a53b42f276db781e15
“sadikturan” <sadikturan@gmail.com>
2023-10-12 23:05:48 +0300

    bilgi güncelleme

--- ProductsAPI/Controllers/ProductsController.cs
+++ ProductsAPI/Controllers/ProductsController.cs
@@ -51,5 +51,37 @@ namespace ProductsAPI.Controllers
 
             return CreatedAtAction(nameof(GetProduct),new { id = entity.ProductId }, entity);
         }
+
+        // localhost:5000/api/products/5 => PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product entity)
+        {
+            if(id != entity.ProductId)
+            {
+                return BadRequest();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            product.ProductName = entity.ProductName;
+            product.Price = entity.Price;
+            product.IsActive = entity.IsActive;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(Exception)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
     }
 }
