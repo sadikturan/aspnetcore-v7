1216662f0279cebf0908858e311fc8dcfef2530c
“sadikturan” <sadikturan@gmail.com>
2023-10-16 09:00:23 +0300

    json web token

--- ProductsAPI/Controllers/UsersController.cs
+++ ProductsAPI/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using ProductsAPI.DTO;
 using ProductsAPI.Models;
 
@@ -11,11 +15,13 @@ namespace ProductsAPI.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _singInManager;
+        private readonly IConfiguration _configuration;
 
-        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _singInManager = signInManager;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -44,6 +50,7 @@ namespace ProductsAPI.Controllers
             BadRequest(result.Errors);
         }
 
+        [HttpPost("login")]
         public async Task<IActionResult> Login(LoginDTO model)
         {
             var user = await _userManager.FindByEmailAsync(model.Email);
@@ -58,10 +65,29 @@ namespace ProductsAPI.Controllers
             if(result.Succeeded)
             {
                 return Ok(
-                    new { token = "token" }
+                    new { token = GenerateJWT(user) }
                 );
             }
             return Unauthorized();
+        }
+
+        private object GenerateJWT(AppUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value ?? "");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                        new Claim(ClaimTypes.Name, user.UserName ?? ""),
+                    }
+                ),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 
