24fe9dea19af9caea7a54254aeb6db62cf6beb1f
“sadikturan” <sadikturan@gmail.com>
2023-10-17 23:14:36 +0300

    Asp.Net Core ile Api Testi

---
+++ aspClientApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using aspClientApp.Models;
+using System.Text.Json;
+
+namespace aspClientApp.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var products = new List<ProductDTO>();
+        
+        using(var httpClient = new HttpClient())
+        {
+            using(var response = await httpClient.GetAsync("http://localhost:5126/api/products"))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                products = JsonSerializer.Deserialize<List<ProductDTO>>(apiResponse);
+            }
+        }
+
+        return View(products);
+
+    }
+
+}
